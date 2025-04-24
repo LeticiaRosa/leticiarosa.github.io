552024977252688c805bca85eb0464ef99084755
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-23 21:22:27 -0300

    Teste

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+export function Header() {
+  return (
+    <div>
+      <div>
+        <a href="http://">Home</a>
+        <a href="http://">About</a>
+        <a href="http://">Projects</a>
+        <a href="http://">Skills</a>
+        <a href="http://">Contact</a>
+      </div>
+      <div>
+        <h1>Hello, I'm a Front-End Developer</h1>
+        <button>
+          VIEW MY WORK
+        </button>
+      </div>
+    </div>
+  );
+}
