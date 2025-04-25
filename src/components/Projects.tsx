5dae928e69e332032b28b4cfff66eea1affb3465
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-24 21:36:14 -0300

    Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 export function Projects() {
   return (
-    <section className="flex h-full w-full flex-row items-center justify-center p-4 lg:p-2">
+    <section className="flex h-full w-full flex-row items-center justify-center pt-8">
       <div className="container mx-auto max-w-250 px-4">
         <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
           <h1 className="text-purple-dark text-3xl font-bold lg:text-5xl">Projects</h1>
