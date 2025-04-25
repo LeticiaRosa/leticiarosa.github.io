df19dfcfdadba3dbea9b16138a0b55d7adb17e9a
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-24 22:04:35 -0300

    fix: ajustando bug no padding

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,13 +2,13 @@ import { CardProjects } from './Card-Projects';
 
 export function Projects() {
   return (
-    <section className="flex h-full w-full flex-row items-center justify-center p-8">
+    <section className="flex h-full w-full flex-row items-center justify-center pt-8">
       <div className="container mx-auto max-w-250 px-4">
-        <div className="flex items-center justify-between gap-2">
-          <h1 className="text-purple-dark text-3xl font-bold lg:text-5xl">Projects</h1>
+        <div className="flex flex-row items-center justify-between gap-2">
+          <h1 className="text-purple-dark px-8 text-3xl font-bold lg:text-5xl">Projects</h1>
         </div>
         <div className="flex flex-col items-start justify-center gap-4 p-4 lg:p-2">
-          <p className="text-s md:text-m text-black">Here are some of my projects:</p>
+          <p className="text-s md:text-m px-4 text-black">Here are some of my projects:</p>
           <div className="flex flex-row flex-wrap items-center justify-center gap-4 p-4 lg:p-2">
             <CardProjects />
             <CardProjects />
