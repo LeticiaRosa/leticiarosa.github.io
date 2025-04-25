4aa45e7171228dd2fdfe87985f819eb5b4ee1b42
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-24 21:58:24 -0300

    Ajuste projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,9 +2,9 @@ import { CardProjects } from './Card-Projects';
 
 export function Projects() {
   return (
-    <section className="flex h-full w-full flex-row items-center justify-center pt-8">
+    <section className="flex h-full w-full flex-row items-center justify-center p-8">
       <div className="container mx-auto max-w-250 px-4">
-        <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
+        <div className="flex items-center justify-between gap-2">
           <h1 className="text-purple-dark text-3xl font-bold lg:text-5xl">Projects</h1>
         </div>
         <div className="flex flex-col items-start justify-center gap-4 p-4 lg:p-2">
