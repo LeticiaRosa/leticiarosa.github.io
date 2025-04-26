0620412537284283367de247d4a45f795f2fb37a
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-26 12:25:03 -0300

    fix: Ajuste de padding no skils

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,39 +3,39 @@ export function Skills() {
     <section className="container-section bg-purple">
       <div className="container">
         <div className="flex flex-row items-center justify-between gap-2">
-          <h1 className="px-8 text-3xl font-bold text-white lg:text-5xl" id="skills">
+          <h1 className="text-3xl font-bold text-white lg:text-5xl" id="skills">
             Skills
           </h1>
         </div>
-        <div className="flex flex-row flex-wrap items-start justify-center gap-8">
-          <div className="items-cente flex flex-col justify-center p-4 lg:p-2">
-            <p className="text-s md:text-m font-bold text-white">Front-End:</p>
-            <li className="text-s md:text-m px-4 text-white">HTML5</li>
-            <li className="text-s md:text-m px-4 text-white">CSS3</li>
-            <li className="text-s md:text-m px-4 text-white">JavaScript</li>
-            <li className="text-s md:text-m px-4 text-white">React.js</li>
-            <li className="text-s md:text-m px-4 text-white">TypeScript</li>
-            <li className="text-s md:text-m px-4 text-white">Tailwind CSS</li>
-            <li className="text-s md:text-m px-4 text-white">Styled-Components</li>
-            <li className="text-s md:text-m px-4 text-white">Next.js</li>
+        <div className="grid grid-cols-1 items-start justify-center gap-8 sm:grid-cols-3">
+          <div className="text-s md:text-m p-4 text-white lg:p-2">
+            <p className="font-bold">Front-End:</p>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>JavaScript</li>
+            <li>React.js</li>
+            <li>TypeScript</li>
+            <li>Tailwind CSS</li>
+            <li>Styled-Components</li>
+            <li>Next.js</li>
           </div>
-          <div className="items-cente flex flex-col justify-center p-4 lg:p-2">
-            <p className="text-s md:text-m font-bold text-white">Tools: </p>
-            <li className="text-s md:text-m px-4 text-white">Git</li>
-            <li className="text-s md:text-m px-4 text-white">GitHub</li>
-            <li className="text-s md:text-m px-4 text-white">GitLab</li>
-            <li className="text-s md:text-m px-4 text-white">VS Code</li>
-            <li className="text-s md:text-m px-4 text-white">Figma</li>
-            <li className="text-s md:text-m px-4 text-white">Vite</li>
-            <li className="text-s md:text-m px-4 text-white">Canva</li>
+          <div className="text-s md:text-m p-4 text-white lg:p-2">
+            <p className="font-bold">Tools: </p>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>GitLab</li>
+            <li>VS Code</li>
+            <li>Figma</li>
+            <li>Vite</li>
+            <li>Canva</li>
           </div>
-          <div className="items-cente flex flex-col justify-center p-4 lg:p-2">
-            <p className="text-s md:text-m font-bold text-white">Soft Skills: </p>
-            <li className="text-s md:text-m px-4 text-white">Comunicação</li>
-            <li className="text-s md:text-m px-4 text-white">Trabalho em equipe</li>
-            <li className="text-s md:text-m px-4 text-white">Proatividade</li>
-            <li className="text-s md:text-m px-4 text-white">Organização</li>
-            <li className="text-s md:text-m px-4 text-white">Aprendizado contínuo</li>
+          <div className="text-s md:text-m p-4 text-white lg:p-2">
+            <p className="font-bold">Soft Skills: </p>
+            <li>Comunicação</li>
+            <li>Trabalho em equipe</li>
+            <li>Proatividade</li>
+            <li>Organização</li>
+            <li>Aprendizado contínuo</li>
           </div>
         </div>
       </div>
