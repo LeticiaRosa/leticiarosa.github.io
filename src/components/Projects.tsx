256173c575bd7070e1145d7346fec609dc914289
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-26 16:07:55 -0300

    Ajustando terceiro projeto

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,16 +3,23 @@ import { CardProjects } from './Card-Projects';
 export function Projects() {
   const projects = [
     {
-      title: 'Service Scheduling System – Marca Aí',
+      title: 'Marcaaí',
       description: `Service scheduling platform developed from scratch with a focus on usability and performance.`,
       imageUrl: '/images/marcaai.png',
       siteUrl: 'https://marcaai-frontend-hm.appmarcaai.com.br',
     },
     {
-      title: 'Wedding Website - Leticia and Gabriel',
-      description: 'I developed an application for my wedding, allowing guests to make contributions via Pix, credit, boleto and debit.',
+      title: 'Wedding Website',
+      description: 'Site for my wedding, allowing guests to make contributions via Pix, credit, boleto and debit.',
       imageUrl: '/images/casamento.png',
       siteUrl: 'https://leticia-gabriel.netlify.app/',
+    },
+    {
+      title: 'Coffee Delivery',
+      description: `Discover the perfect coffee experience with Coffee Delivery!
+      Select your favorite blends, customize your order, and enjoy premium coffee delivered straight to your home.`,
+      imageUrl: '/images/coffee shop.png',
+      siteUrl: 'https://shop-coffee-delivery.netlify.app/',
     },
   ];
 
@@ -23,7 +30,7 @@ export function Projects() {
           Projects
         </h1>
         <p className="text-s md:text-m text-black">Here are some of my projects:</p>
-        <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-3">
           {projects.map((project, index) => (
             <CardProjects key={index} title={project.title} description={project.description} imageUrl={project.imageUrl} siteUrl={project.siteUrl} />
           ))}
