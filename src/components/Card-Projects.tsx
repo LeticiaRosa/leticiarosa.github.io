09f52cd55492e55af52e900f2a2bfbe5fe38baf6
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-25 22:26:35 -0300

    adicionando projetos

--- src/components/Card-Projects.tsx
+++ src/components/Card-Projects.tsx
@@ -1,17 +1,20 @@
-import foguete from '../../public/images/foguete.png';
+interface CardProjectsProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+  siteUrl: string;
+}
 
-export function CardProjects() {
+export function CardProjects({ title, description, imageUrl, siteUrl }: CardProjectsProps) {
   return (
-    <div className="bg-purple-light flex h-80 w-60 flex-col items-center gap-2 rounded-lg">
-      <div className="bg-purple-pink flex h-35 w-full items-start justify-center rounded-lg rounded-b-none">
-        <div className="flex w-full items-center justify-center max-[300px]:hidden">
-          <img src={foguete} className="h-35 object-contain" alt="Foguete" />
+    <div onClick={() => window.open(siteUrl, '_blank')} className="bg-purple-light flex h-100 w-80 flex-col items-center gap-2 rounded-lg">
+      <div className="bg-purple-pink flex h-50 w-full items-start justify-center rounded-lg rounded-b-none">
+        <div className="flex h-full w-full items-center justify-center max-[300px]:hidden">
+          <img src={imageUrl} className="h-full w-full rounded-t-lg object-cover" alt={title} />
         </div>
       </div>
-      <h1 className="text-purple-dark text-l font-bold">Project Name</h1>
-      <p className="text-s line-clamp-5 overflow-hidden px-4 text-ellipsis text-black">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-      </p>
+      <h1 className="text-purple-dark text-m text-center font-bold">{title}</h1>
+      <p className="text-s line-clamp-5 overflow-hidden px-4 text-ellipsis text-black">{description}</p>
     </div>
   );
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,20 +1,33 @@
 import { CardProjects } from './Card-Projects';
 
 export function Projects() {
+  const projects = [
+    {
+      title: 'Sistema de Agendamento de Serviços – Marca Aí',
+      description: `Plataforma de agendamento de serviços desenvolvida do zero com foco em usabilidade e performance.`,
+      imageUrl: '/images/marcaai.png',
+      siteUrl: 'https://marcaai-frontend-hm.appmarcaai.com.br',
+    },
+    {
+      title: 'Site de Casamento - Letícia e Gabriel',
+      description: 'Desenvolvi uma aplicação para o meu casamento, permitindo aos convidados realizar contribuições via Pix, crédito, boleto e débito.',
+      imageUrl: '/images/casamento.png',
+      siteUrl: 'https://leticia-gabriel.netlify.app/',
+    },
+  ];
+
   return (
     <section className="flex h-full w-full flex-row items-center justify-center pt-8">
       <div className="container mx-auto max-w-250 px-4">
         <div className="flex flex-row items-center justify-between gap-2">
           <h1 className="text-purple-dark px-8 text-3xl font-bold lg:text-5xl">Projects</h1>
         </div>
-        <div className="flex flex-col items-start justify-center gap-4 p-4 lg:p-2">
+        <div className="items-cente flex flex-col justify-center gap-4 p-4 lg:p-2">
           <p className="text-s md:text-m px-4 text-black">Here are some of my projects:</p>
           <div className="flex flex-row flex-wrap items-center justify-center gap-4 p-4 lg:p-2">
-            <CardProjects />
-            <CardProjects />
-            <CardProjects />
-            <CardProjects />
-            <CardProjects />
+            {projects.map((project, index) => (
+              <CardProjects key={index} title={project.title} description={project.description} imageUrl={project.imageUrl} siteUrl={project.siteUrl} />
+            ))}
           </div>
         </div>
       </div>
