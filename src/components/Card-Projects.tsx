5f30decc54391f7c3157e02a81e72999132e6159
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-04-26 16:52:21 -0300

    Ajustando cards projects

--- src/components/Card-Projects.tsx
+++ src/components/Card-Projects.tsx
@@ -8,8 +8,8 @@ interface CardProjectsProps {
 export function CardProjects({ title, description, imageUrl, siteUrl }: CardProjectsProps) {
   return (
     <div className="flex flex-col rounded-xl bg-white shadow-lg transition-all duration-300 hover:shadow-2xl">
-      <img src={imageUrl} alt={title} className="h-60 w-full rounded-t-xl object-cover" />
-      <div className="flex flex-grow flex-col justify-between p-6">
+      <img src={imageUrl} alt={title} className="h-30 w-full rounded-t-xl object-scale-down" />
+      <div className="flex flex-grow flex-col justify-between p-6 pt-0">
         <div>
           <h3 className="text-2xl font-semibold text-purple-700">{title}</h3>
           <p className="mt-2 text-gray-600">{description}</p>
